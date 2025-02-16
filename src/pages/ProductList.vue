a05107e373d82e7be9dc6626319a60a5018876f7
hardiksavani35 <hardiksavaniaus@gmail.com>
2025-02-16 14:27:50 +1030

    - Implement filter

--- src/pages/ProductList.vue
+++ src/pages/ProductList.vue
@@ -6,23 +6,49 @@
         </div>
 
         <!-- Product List -->
-        <div class="container pb-5 mb-3">
-            <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-                <div class="col-md-4" v-for="product in products" :key="product.id">
-                    <div class="card mb-4 rounded-3 shadow-sm">
-                        <div class="card-header py-3">
-                            <h4 class="my-0 fw-normal">{{ product.title }}</h4>
+        <div class="container mb-5">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="sticky-top" style="top: 80px;">
+                        <h4 class="fw-bold">Filters</h4>
+                        <!-- Filter options (You can add your filter options here) -->
+                        <div class="mb-3">
+                            <label for="category" class="form-label">Category</label>
+                            <select id="category" class="form-select" v-model="selectedCategory">
+                                <option value="">Select Category</option>
+                                <option v-for="category in categories" :key="category" :value="category">{{category}}</option> 
+                            </select>
                         </div>
-                        <div class="card-body">
-                            <img :src="product.url" width="100%" alt="Product Image">
+                        <div class="mb-3">
+                            <label for="price" class="form-label">Price</label>
+                            <select id="price" class="form-select" v-model="selectedPriceOrder">
+                                <option value="">Default</option>
+                                <option option="lowtohigh">Low to High</option> 
+                                <option option="hightolow">High to Low</option> 
+                            </select>
                         </div>
-                        <div class="card-footer d-flex justify-content-between">
-                            <p class="text-start">{{ product.price }}$</p>
-                            <p class="text-end">
-                                <router-link :to="{ name: 'product-details', params: { id: product.id } }">
-                                    Details
-                                </router-link>
-                            </p>
+                        <!-- Add more filters as needed -->
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+                        <div class="col-md-4" v-for="product in products" :key="product.id">
+                            <div class="card mb-4 rounded-3 shadow-sm">
+                                <div class="card-header py-3">
+                                    <h6 class="my-0 fw-normal">{{ product.title }}</h6>
+                                </div>
+                                <div class="card-body">
+                                    <img :src="product.url" width="100%" alt="Product Image">
+                                </div>
+                                <div class="card-footer d-flex justify-content-between">
+                                    <p class="text-start">{{ product.price }}$</p>
+                                    <p class="text-end">
+                                        <router-link :to="{ name: 'product-details', params: { id: product.id } }">
+                                            Details
+                                        </router-link>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -33,11 +59,26 @@
 
 <script>
 export default {
+    data() {
+        return {
+            selectedCategory: "",
+            selectedPriceOrder: "" 
+        }
+    },
     computed: {
         products() {
-            return this.$store.getters['products/products'];
+            let productsData = this.$store.getters['products/products'];
+            
+            if (this.selectedCategory) {
+                productsData = productsData.filter(product => product.category === this.selectedCategory);
+            }
+
+            return productsData;
+        },
+        categories() {
+            return this.$store.getters['products/getCategories']; 
         }
-    }
+    }, 
 }
 </script>
 
--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -3,13 +3,19 @@ export default {
     state() {
         return {
             products: [
-                { id: 1, title: "Product 1", url: "/products/product.jpg", price: 10.25, description: "Test description" },
-                { id: 2, title: "Product 2", url: "/products/product.jpg", price: 15.50, description: "Another test description" },
-                { id: 3, title: "Product 3", url: "/products/product.jpg", price: 20.75, description: "Yet another test description" },
-                { id: 4, title: "Product 4", url: "/products/product.jpg", price: 25.00, description: "Fourth product description" },
-                { id: 5, title: "Product 4", url: "/products/product.jpg", price: 25.00, description: "Fourth product description" },
-                { id: 6, title: "Product 4", url: "/products/product.jpg", price: 25.00, description: "Fourth product description" },
-            ]
+                { id: 1, title: "Zipper Repair Kit", url: "/products/product.jpg", price: 10.25, description: "Zipper Repair Kit Zipper Head for Clothing Bags Universal Zipper Repair Kit", category: "Bag" },
+                { id: 2, title: "Zipper Pull Replacement Metal", url: "/products/product.jpg", price: 15.50, description: "Another test description", category: "Bag" },
+                { id: 3, title: "Luggage Label (1 PCS)", url: "/products/product.jpg", price: 20.75, description: "Luggage Label Luggage Tags Suitcase Tags Name Tags for Bags", category: "Safety Products" },
+                { id: 4, title: "Document Organizer Bag", url: "/products/product.jpg", price: 25.00, description: "Document Organizer Bag Multilayer Travel File Organiser Waterproof Document Storage Bag", category: "Bag" },
+                { id: 5, title: "Laptop Backpack", url: "/products/product.jpg", price: 25.00, description: "A cool laptop backpack", category: "Summer Class" },
+                { id: 6, title: "Casual T-Shirt for Men", url: "/products/product.jpg", price: 5.00, description: "Slim-fitting style, t-shoty for men", category: "Summer Class" },
+                { id: 7, title: "Mens Cotton Jacket", url: "/products/product.jpg", price: 20.25, description: "Great outerwear jackets for spring, autumn, or winter.", category: "Home Decor" },
+                { id: 8, title: "Mens Casual Slim Fit 2", url: "/products/product.jpg", price: 15.50, description: "An aweome shirt for men", category: "Wallpaper" },
+                { id: 9, title: "Women's Gold & Silver Bracelet", url: "/products/product.jpg", price: 2.75, description: "A very cool bracelet.", category: "Wallpaper" },
+                { id: 10, title: "Solid Gold Petite Micropave", url: "/products/product.jpg", price: 25.00, description: "A very cool jewlery for women", category: "Bag" },
+                { id: 11, title: "White Gold Plated Princess", url: "/products/product.jpg", price: 9.99, description: "A great diamond engagement ring for her.", category: "Safety Products" },
+                { id: 12, title: "Gold-plated Earrings", url: "/products/product.jpg", price: 10.99, description: "Rose Gold Plated Double Flared Tunnel Plug Earrings.", category: "Home Decor" },
+            ], 
         }
     },
     mutations: {
